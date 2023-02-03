eeee202b64db0b9c705f10e8baf3aa9253ade4b7
Nilton Cesar <ncpma.ing@gmail.com>
2023-02-02 23:28:30 -0500

    modificado .htaccess

--- app/views/pages/login.php
+++ app/views/pages/login.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <script src="https://kit.fontawesome.com/64d58efce2.js" crossorigin="anonymous"></script>
+    
     <link rel="stylesheet" href="<?php echo URLROOT; ?>/css/login.css" />
     <title>SAC CLONSA INGENIERIA </title>
 </head>
