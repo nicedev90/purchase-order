95fd850cdc97c11966c7b5fa3e7f08c1382cbb8d
RICKSOR21 <jtunoquesa@unprg.edu.pe>
2023-02-19 22:21:26 -0500

    Subiendo

--- app/config/config.php
+++ app/config/config.php
@@ -7,4 +7,13 @@
 	define('APPROOT', dirname(dirname(__FILE__)));
 	define('URLROOT', 'http://localhost/purchase-order');
 	define('APPNAME', 'SAC CLONSA');
-?>
+
+//	define('DB_HOST', '190.107.177.60');
+//	define('DB_USER', 'ccl39382_sac');
+//	define('DB_PASS', 'sac2023');
+//	define('DB_NAME', 'ccl39382_ordenes');
+
+//	define('APPROOT', dirname(dirname(__FILE__)));
+//	define('URLROOT', 'https://clonsaingenieria.cl/purchase-order');
+//	define('APPNAME', 'SAC CLONSA');
+?> 
