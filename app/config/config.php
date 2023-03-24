a196602f64a3cd9562882c786e8e8a0cbb709ed5
Nilton Cesar <ncpma.ing@gmail.com>
2023-03-24 08:53:17 -0500

    editar funcion

--- app/config/config.php
+++ app/config/config.php
@@ -1,9 +1,9 @@
 <?php 
 	define('DB_HOST', '127.0.0.1');
 	define('DB_USER', 'root');
-	define('DB_PASS', '');
+	// define('DB_PASS', '');
 	define('DB_PASS', 'root');
-	// define('DB_NAME', 'ordenes1');
+	define('DB_NAME', 'ordenes1');
 
 	define('APPROOT', dirname(dirname(__FILE__)));
 	// define('URLROOT', 'http://192.168.8.100/purchase-order');
