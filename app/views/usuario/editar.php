3ff69996854f6ca2b17db918fa76817e16516db0
Nilton Cesar <ncpma.ing@gmail.com>
2023-03-27 14:18:38 -0500

    fy editar[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[D[C[C[C[C[C[C[C[3~

--- app/views/usuario/editar.php
+++ app/views/usuario/editar.php
@@ -378,15 +378,26 @@
           </div>
           
 
-              <div class="d-md-flex alert alert-success text-dark ">
-                <?php for ($i = 1; $i <= count($data['adjuntos']) -1; $i++) : ?> 
-                  <div class="row col-md-4 p-4">
-                    <img src="<?= URLROOT . $data['adjuntos'][$i]->archivo ?>" style="width:40%" class="img-thumbnail">
-                    <a href="<?= URLROOT . $data['adjuntos'][$i]->archivo ?>" target="_blank"> Ver Completo  </a>
-                  </div>
-                <?php endfor; ?>
-              </div>
+          <div class="d-md-flex alert alert-success text-dark ">
+            <?php if (str_contains($data['adjuntos'][0]->archivo, '.')) : ?>
+              <?php for ($i = 1; $i <= count($data['adjuntos']); $i++) : ?> 
+                <div class="row col-md-4 p-4">
+                  <img src="<?= URLROOT . $data['adjuntos'][$i]->archivo ?>" style="width:40%" class="img-thumbnail">
+                  <a href="<?= URLROOT . $data['adjuntos'][$i]->archivo ?>" target="_blank"> Ver Completo  </a>
+                </div>
+              <?php endfor; ?>
+            <?php else : ?>
 
+              <?php for ($i = 1; $i <= count($data['adjuntos']) -1; $i++) : ?> 
+                <div class="row col-md-4 p-4">
+                  <img src="<?= URLROOT . $data['adjuntos'][$i]->archivo ?>" style="width:40%" class="img-thumbnail">
+                  <a href="<?= URLROOT . $data['adjuntos'][$i]->archivo ?>" target="_blank"> Ver Completo  </a>
+                </div>
+              <?php endfor; ?>
+
+            <?php endif; ?>
+
+          </div>
           
         </div>
 
